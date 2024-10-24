5822f706b470ee02e0668b3c6ecaea490f251000
David29483 <d.ferreiradocarmo49@gmail.com>
2024-10-24 12:06:42 +0200

    Tkiteasy1

--- Calculatrice.py
+++ Calculatrice.py
@@ -23,11 +23,28 @@ addition_texte = g.afficherTexte("+",255,525,"pink")
 soustraction = g.dessinerRectangle(331,500,49,49,"white")
 soustraction_texte = g.afficherTexte("-",355,525,"pink")
 
-valeur1 = g.dessinerRectangle(30,300,100,100,"blue")
-valeur2 = g.dessinerRectangle(150,300,100,100,"blue")
+affiche_valeur1 = g.dessinerRectangle(30,300,100,100,"blue")
+affiche_valeur2 = g.dessinerRectangle(150,300,100,100,"blue")
 
 affiche_resultat = g.dessinerRectangle(100,100,200,100,"white")
 
+valeur1 = int(input("Donnez le premier chiffre"))
 g.attendreClic()
 g.actualiser()
 
+while clic != (multiplication or division or addition or soustraction) :
+    g.recupererClic()
+
+valeur2 = int(input("Donnez le deuxième chiffre"))
+
+if clic == multiplication :
+    g.afficherImage(f"{valeur1} {multiplication_texte} {valeur2} = {resultat}")
+
+if clic == addition:
+    g.afficherImage(f"{valeur1} {addition_texte} {valeur2} = {resultat}")
+
+if clic == division :
+    g.afficherImage(f"{valeur1} {division_texte} {valeur2} = {resultat}")
+
+if clic == soustraction :
+    g.afficherImage(f"{valeur1} {soustraction_texte} {valeur2} = {resultat}")
