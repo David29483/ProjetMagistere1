33100a714606f98aa3a5b00bdd2c765e3e43096b
David29483 <d.ferreiradocarmo49@gmail.com>
2024-10-30 19:22:24 +0100

    Premiere version

--- Calculatrice.py
+++ Calculatrice.py
@@ -11,20 +11,23 @@ Created on Fri Sep 20 20:38:27 2024
 
 from tkiteasy import *
 
+class Calculatrice():
+    def __init__(self):
+        g = ouvrirFenetre(400,600)
 
+    def initGraph(self):
+        multiplication = g.dessinerRectangle(30,500,49,49,"white")
+        multiplication_texte = g.afficherTexte("x",54,525,"pink")
 
-g = ouvrirFenetre(400,600)
-multiplication = g.dessinerRectangle(30,500,49,49,"white")
-multiplication_texte = g.afficherTexte("x",54,525,"pink")
+        division = g.dessinerRectangle(131,500,49,49,"white")
+        division_texte = g.afficherTexte("/",155,525,"pink")
 
-division = g.dessinerRectangle(131,500,49,49,"white")
-division_texte = g.afficherTexte("/",155,525,"pink")
+        addition = g.dessinerRectangle(231,500,49,49,"white")
+        addition_texte = g.afficherTexte("+",255,525,"pink")
 
-addition = g.dessinerRectangle(231,500,49,49,"white")
-addition_texte = g.afficherTexte("+",255,525,"pink")
+        soustraction = g.dessinerRectangle(331,500,49,49,"white")
+        soustraction_texte = g.afficherTexte("-",355,525,"pink")
 
-soustraction = g.dessinerRectangle(331,500,49,49,"white")
-soustraction_texte = g.afficherTexte("-",355,525,"pink")
 
 affiche_valeur1 = g.dessinerRectangle(30,300,100,100,"blue")
 affiche_valeur2 = g.dessinerRectangle(150,300,100,100,"blue")
