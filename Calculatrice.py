3340b26668558006f1658f68d5ee330c52770252
David29483 <d.ferreiradocarmo49@gmail.com>
2024-10-31 20:14:08 +0100

    Premiere version qui marche mais vraiment pas efficiente

--- Calculatrice.py
+++ Calculatrice.py
@@ -11,46 +11,70 @@ Created on Fri Sep 20 20:38:27 2024
 
 from tkiteasy import *
 
+
 class Calculatrice():
     def __init__(self):
-        g = ouvrirFenetre(400,600)
+        self.g = ouvrirFenetre(400, 600)
+        self.g.dessinerRectangle(30, 500, 49, 49, "white")
 
     def initGraph(self):
-        multiplication = g.dessinerRectangle(30,500,49,49,"white")
-        multiplication_texte = g.afficherTexte("x",54,525,"pink")
+        self.multiplication = self.g.dessinerRectangle(30, 500, 49, 49, "white")
+        self.multiplication_texte = self.g.afficherTexte("x", 54, 525, "pink")
 
-        division = g.dessinerRectangle(131,500,49,49,"white")
-        division_texte = g.afficherTexte("/",155,525,"pink")
+        self.division = self.g.dessinerRectangle(131, 500, 49, 49, "white")
+        self.division_texte = self.g.afficherTexte("/", 155, 525, "pink")
 
-        addition = g.dessinerRectangle(231,500,49,49,"white")
-        addition_texte = g.afficherTexte("+",255,525,"pink")
+        self.addition = self.g.dessinerRectangle(231, 500, 49, 49, "white")
+        self.addition_texte = self.g.afficherTexte("+", 255, 525, "pink")
 
-        soustraction = g.dessinerRectangle(331,500,49,49,"white")
-        soustraction_texte = g.afficherTexte("-",355,525,"pink")
+        self.soustraction = self.g.dessinerRectangle(331, 500, 49, 49, "white")
+        self.soustraction_texte = self.g.afficherTexte("-", 355, 525, "pink")
+
+        self.affiche_valeur1 = self.g.dessinerRectangle(30, 300, 100, 100, "blue")
+        self.affiche_valeur2 = self.g.dessinerRectangle(150, 300, 100, 100, "blue")
+
+        self.affiche_resultat = self.g.dessinerRectangle(100, 100, 200, 100, "white")
+        self.g.actualiser()
 
 
-affiche_valeur1 = g.dessinerRectangle(30,300,100,100,"blue")
-affiche_valeur2 = g.dessinerRectangle(150,300,100,100,"blue")
-
-affiche_resultat = g.dessinerRectangle(100,100,200,100,"white")
-
+calculatrice = Calculatrice()
+calculatrice.initGraph()
 valeur1 = int(input("Donnez le premier chiffre"))
-g.attendreClic()
-g.actualiser()
+valeur1_texte = calculatrice.g.afficherTexte(valeur1, 75, 350, "white")
+calculatrice.g.actualiser()
 
-while clic != (multiplication or division or addition or soustraction) :
-    g.recupererClic()
+calcul = None
+clic = None
+
+while calcul != calculatrice.multiplication and calcul != calculatrice.division and calcul != calculatrice.addition and calcul != calculatrice.soustraction:
+    clic = calculatrice.g.attendreClic()
+    calcul = calculatrice.g.recupererObjet(clic.x, clic.y)
 
 valeur2 = int(input("Donnez le deuxième chiffre"))
+valeur2_texte = calculatrice.g.afficherTexte(valeur2, 200, 350, "white")
 
-if clic == multiplication :
-    g.afficherImage(f"{valeur1} {multiplication_texte} {valeur2} = {resultat}")
+if calcul == calculatrice.multiplication:
+    resultat = valeur1 * valeur2
+    print(resultat)
+    calculatrice.g.afficherTexte(f"{valeur1} x {valeur2} = {resultat}", 150, 150, "red")
 
-if clic == addition:
-    g.afficherImage(f"{valeur1} {addition_texte} {valeur2} = {resultat}")
+if calcul == calculatrice.addition:
+    resultat = valeur1 + valeur2
+    print(resultat)
 
-if clic == division :
-    g.afficherImage(f"{valeur1} {division_texte} {valeur2} = {resultat}")
+    calculatrice.g.afficherTexte(f"{valeur1} + {valeur2} = {resultat}", 150, 150, "red")
 
-if clic == soustraction :
-    g.afficherImage(f"{valeur1} {soustraction_texte} {valeur2} = {resultat}")
+if calcul == calculatrice.division:
+    resultat = valeur1 / valeur2
+    print(resultat)
+
+    calculatrice.g.afficherTexte(f"{valeur1} / {valeur2} = {resultat}", 150, 150, "red")
+
+if calcul == calculatrice.soustraction:
+    resultat = valeur1 - valeur2
+    print(resultat)
+
+    calculatrice.g.afficherTexte(f"{valeur1} - {valeur2} = {resultat}", 150, 150, "red")
+
+calculatrice.g.actualiser()
+clic = calculatrice.g.attendreClic()
